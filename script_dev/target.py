6dc19d912364bfc9f0b03494d7a441432770c7e4
Joe Napoli <JNapoli@stanford.edu>
2015-06-30 14:06:46 -0700

    First commit for reax project

---
+++ script_dev/target.py
@@ -0,0 +1,23 @@
+from itertools import islice
+import sys
+
+class Target:
+    def __init__(self, fn_train):
+        self.poses = self.load_poses(fn_train)
+        print(len(self.poses))
+
+    def load_poses(self, fn_train):
+        poses = []
+        with open(fn_train,'r') as f:
+            raw_dat = [line.strip() for line in f.readlines()]
+        len_chunk = int(raw_dat[0]) + 2
+        start = 0    
+        while True:
+            if start == len(raw_dat): break
+            lines = raw_dat[start:start+len_chunk]
+            e_in_Ha = float(lines[1])
+            coord_lines = lines[2:]
+            h2o_atom_coords = [tuple(line.split()[1:]) for line in coord_lines]
+            poses.append((h2o_atom_coords, e_in_Ha))
+            start += len_chunk
+        return poses
