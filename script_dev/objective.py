5051e9956782d4c4780859e7c1fbf2cfc2bd82f2
Joe Napoli <JNapoli@stanford.edu>
2015-07-08 16:28:42 -0700

    prepare_coords() method is no longer necessary.

--- script_dev/objective.py
+++ script_dev/objective.py
@@ -4,32 +4,19 @@ import numpy as np
 import lmps_interact
 import os
 import target
-
-
-def prepare_coords(x, pose_coords):
-    """
-    Function for filling the ctype array
-    with values for each d.o.f.
-    """
-    curr = 0
-
-    for atom in pose_coords:
-        for dof in atom:
-            x[curr] = dof
-            curr += 1
+import unit
 
 
 class Objective(object):
-    def __init__(self, target, lmp_engine):
+    def __init__(self, tar, lmp_engine):
         """
         Compute objective function, given an engine instance
         and target configuration series.
         """
-        self._tar = target
-        self._lmp = lmp_engine
-        self._monomer_in = ''
-        self._monomer_data = ''
-        self.X2 = self.compute_X2()
+        self._tar     = tar
+        self._lmp     = lmp_engine
+        self._job_dir = os.getcwd()
+        self.X2       = self.compute_X2()
 
     def compute_X2(self):
 
@@ -39,54 +26,45 @@ class Objective(object):
         e_series = []
 
         if self._tar.series_type == 'monomer':
-            # Loop over the target's poses
-            # and extract eneries from lmp_engine
-            x_tmp = (N * ct.c_double)()
 
             for pose in self._tar.poses:
-                prepare_coords(x_tmp, pose[0])
-                lmp.update(x_tmp)
-                e_series.append(lmp.V)
+                e_series.append(lmp.get_V(pose[0].reshape((1,-1))))
 
         elif self._tar.series_type == 'dimer':
             # Requires auxiliary engine
             # for monomer calculations
-            #lmp_mono = #instantiate here
+            os.chdir(os.path.join(os.getcwd(), 'monomer_files'))
+            lmp_mono = lmps_interact.LAMMPS('in.water')
 
-            x_tmp_monomer = (9 * ct.c_double)()
-            x_tmp_dimer = (N * ct.c_double)()
             e_m1 = []
             e_m2 = []
-            e_d = []
+            e_dimer = []
 
             for pose in self._tar.poses:
                 coords = pose[0]
                 h2o1_coords = coords[:3]
                 h2o2_coords = coords[3:]
 
-                # Compute each monomer separately
-                prepare_coords(x_tmp_monomer, h2o1_coords)
-                lmp_mono.update(x_tmp_monomer)
-                e_m1.append(lmp_mono.V)
-
-                prepare_coords(x_tmp_monomer, h2o2_coords)
-                lmp_mono.update(x_tmp_monomer)
-                e_m2.append(lmp_mono.V)
+                # Energy we want is the 2-body energy, so compute
+                # each monomer separately.
+                e_m1.append(lmp_mono.get_V(h2o1_coords.reshape((1,-1))[0]))
+                e_m2.append(lmp_mono.get_V(h2o2_coords.reshape((1,-1))[0]))
 
                 # Dimer
-                prepare_coords(x_tmp_dimer, coords)
-                lmp.update(x_tmp_dimer)
-                e_d.append(lmp.V)
+                e_dimer.append(lmp.get_V(coords.reshape((1,-1))[0]))
 
-            e_2body  = e_d - e_m1
+            e_m1 = np.array(e_m1)
+            e_m2 = np.array(e_m2)
+            e_dimer = np.array(e_dimer)
+            e_2body  = e_dimer - e_m1
             e_2body -= e_m2
             e_series = e_2body
+            os.chdir(self._job_dir)
 
         else:
             pass
 
         e_series = np.array(e_series)
-        # TODO: make X2 contribution coefficients more general
-        X2 = sum(((e_series - target.energies) / unit.kT )**2) 
+        X2 = sum(((e_series - self._tar.energies) / unit.kT )**2) 
 
         return X2
